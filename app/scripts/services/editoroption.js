be3c3e52fad3f7fcde1407a8a63b9cb8bc0feb68
Joshua Jung <wbqdinno@gmail.com>
2016-12-12 06:26:14 +0900

    TinyMCE를 CDN에서 서빙받도록 변경

--- app/scripts/services/editoroption.js
+++ app/scripts/services/editoroption.js
@@ -11,7 +11,7 @@ angular.module('primeApp')
   .constant('editorOption', {
     plugins: 'autoresize contextmenu fullscreen hr link image imagetools code table advlist autolink lists charmap print preview paste searchreplace textcolor',
     toolbar: 'undo redo paste searchreplace | bold italic forecolor backcolor | alignleft aligncenter alignright | image table | code fullscreen',
-    language: 'ko_KR',
+    language_url: 'https://firebasestorage.googleapis.com/v0/b/prime-426ec.appspot.com/o/ko_KR.js?alt=media&token=b026d1c7-f022-4c35-a8d5-f1415ba54571',
     autoresize_max_height: 600,
     contextmenu: "link image inserttable | cell row column deletetable",
     image_caption: true,
